419e668d4763a9ed0d29b754c5be696b2b2b58ed
Shu Sian Lyu <shusianlyu@gmail.com>
2023-06-22 13:58:37 -0700

    Add sky conditions

--- backend.py
+++ backend.py
@@ -3,7 +3,7 @@ import requests
 API_KEY = "8de3f84a8f948ebd96c24db4c11b39ee"
 
 
-def get_data(place, forecast_days, kind):
+def get_data(place, forecast_days):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid" \
           f"={API_KEY}"
     response = requests.get(url)
@@ -11,12 +11,9 @@ def get_data(place, forecast_days, kind):
     filtered_data = res['list']
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [data['main']['temp'] for data in filtered_data]
-    if kind == "Sky":
-        filtered_data = [data['weather'][0]['main'] for data in filtered_data]
+
     return filtered_data
 
 
 if __name__ == '__main__':
-    print(get_data("Tokyo", 2, 'Sky'))
+    print(get_data("Tokyo", 2))
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from backend import get_data
 
 st.set_page_config(layout="wide")
 
+# Add title, text input, slider, selectbox, and subheader
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
@@ -11,8 +12,21 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view", options=["Temperature", "Sky"])
 st.subheader(f"{option} for the next {days} days in {place}")
 
+if place:
+    # Get the temperature/sky data
+    sky_conditions = get_data(place, days)
 
-date = get_data(place, days, option)
+    if option == 'Temperature':
+        # Create a temperature plot
+        temperatures = [data['main']['temp'] for data in sky_conditions]
+        dates = [data['dt_txt'] for data in sky_conditions]
+        figure = px.line(x=dates, y=temperatures,
+                         labels={"x": "Date", "y": "Temperature (C)"})
+        st.plotly_chart(figure)
 
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+    if option == 'Sky':
+        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                  "Rain": "images/rain.png", "Snow": "images/snow.png"}
+        sky_conditions = [data['weather'][0]['main'] for data in sky_conditions]
+        img_paths = [images[condition] for condition in sky_conditions]
+        st.image(img_paths, width=115)
