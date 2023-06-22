542b00130af4953f84fa68fef35b2e11ee21d4ed
Shu Sian Lyu <shusianlyu@gmail.com>
2023-06-21 23:39:39 -0700

    Plot fake data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.express as px
 
 st.set_page_config(layout="wide")
 
@@ -8,3 +9,16 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Select the number of forecast days")
 option = st.selectbox("Select data to view", options=["Temperature", "Sky"])
 st.subheader(f"{option} for the next {days} days in {place}")
+
+
+def get_data(days):
+    dates = ["2022-10-23", "2022-10-24", "2022-10-25"]
+    temperatures = [11, 15, 13]
+    temperatures = [days * i for i in temperatures]
+    return dates, temperatures
+
+
+dates, temperatures = get_data(days)
+figure = px.line(x=dates, y=temperatures,
+                 labels={"x": "Date", "y": "Temperature (C)"})
+st.plotly_chart(figure)
